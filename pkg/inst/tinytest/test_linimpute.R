2a05cf2ffac706b0225d3b3f897573284c7df657
Mark van der Loo <mark.vanderloo@gmail.com>
2021-04-01 11:04:12 +0200

    added regressiontest

--- pkg/inst/tinytest/test_linimpute.R
+++ pkg/inst/tinytest/test_linimpute.R
@@ -192,7 +192,11 @@ d_out <- data.frame(x=c(NA_real_,1), y=c(NA_real_,2))
 rules <- validator(x>=0, y>=x, y - 2*x == 0)
 expect_equal(impute_lr(d_in,rules), d_out, info="all d[1,] missing")
 
-
+## regression test, thanks to Sigrid van Hoek for reporting the issue.
+d <- read.csv("lr_implied_data.csv")
+r <- validator(.file="lr_implied_rules.yml")
+expect_true( all(confront(impute_lr(d,r, methods="implied"), r), na.rm=TRUE))
+expect_true( all(confront(impute_lr(d,r, methods="fm"), r), na.rm=TRUE))
 
 
 
